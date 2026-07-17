66de3e7c9e5b48f668522011c8a7ebf45bb3148f
Prashant-Khatri <prashantkhatri20170215974@gmail.com>
2026-07-17 20:33:02 +0530

    Deploy console-2

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,5 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL
+console.log("Backend url is : ",SOCKET_URL)
 declare global {
   var globalSocket: Socket | undefined;
 }
