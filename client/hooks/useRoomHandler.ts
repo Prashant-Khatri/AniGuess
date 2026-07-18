327e415cd0fdc89d6f6cfa349a80b8f9c8499430
Prashant-Khatri <prashantkhatri20170215974@gmail.com>
2026-07-18 13:26:37 +0530

    Deployment success

--- client/hooks/useRoomHandler.ts
+++ client/hooks/useRoomHandler.ts
@@ -3,10 +3,7 @@ import { Socket } from 'socket.io-client';
 export const useRoomHandler = (socket: Socket) => {
   
   const createRoom = (userName: string, avatarId: number, userId: string) => {
-    console.log("I am inside useRoomHandler.ts : ",userName,avatarId,userId)
-    console.log("Socket is inside createRoom : ",socket.id)
     if (!socket.connected) socket.connect();
-    console.log("Going to emit : ",socket.id)
     socket.emit('create_room', { userName, avatarId, userId });
   }
 
